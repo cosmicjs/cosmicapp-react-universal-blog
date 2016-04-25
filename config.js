effb8f360c0984cf6a8fcf31f4f36e4601b2acbf
Itagiba <lilian@heydancers.com>
2016-04-25 18:54:58 +0100

    Changed to point to hey-dancers

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ export default {
     title: 'React Universal Blog'
   },
   bucket: {
-    slug: process.env.COSMIC_BUCKET || 'react-universal-blog',
+    slug: process.env.COSMIC_BUCKET || 'hey-dancers',
     media_url: 'https://cosmicjs.com/uploads'
   }
 }
