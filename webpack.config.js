38cc2bbec21e34746aa2fba3e17f81eb244d9523
lilian.couto <lilian@heydancers>
2016-05-09 11:59:33 +0100

    added svg images and removed boostrap

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,8 @@
 // webpack.config.js
 var webpack = require('webpack')
+//copy files
+var CopyWebpackPlugin = require('copy-webpack-plugin');
+var path = require('path');
 
 if(process.env.NODE_ENV === 'development'){
   var loaders = ['react-hot','babel']
@@ -8,26 +11,36 @@ if(process.env.NODE_ENV === 'development'){
 }
 module.exports = {
   devtool: 'eval',
+  //bundle app from here
   entry: './app-client.js',
   output: {
     path: __dirname + '/public/dist',
+    //output it to /public/dist/bundle.js
     filename: 'bundle.js',
     publicPath: '/dist/'
   },
+  devServer: {
+    // This is required for webpack-dev-server. The path should
+    // be an absolute path to your build destination.
+    outputPath: path.join(__dirname +  '/public/')
+},
   module: {
     loaders: [{
       test: /\.js$/,
       loaders: loaders,
       exclude: /node_modules/
     },
-    { 
-     test: /\.scss$/, 
-     loaders: ["style", "css", "sass"] 
+    {
+     test: /\.scss$/,
+     loaders: ["style", "css", "sass"]
     }
   ]},
   plugins: [
     new webpack.DefinePlugin({
       'process.env.COSMIC_BUCKET': JSON.stringify(process.env.COSMIC_BUCKET)
-    })
+    }),
+    new CopyWebpackPlugin([
+      { from: 'images/', to: 'img/' }
+    ])
  ]
 };
